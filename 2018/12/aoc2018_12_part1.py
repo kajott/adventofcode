9507806e5314965871450a98f5581bf89c15b62a
Martin Fiedler <keyj@emphy.de>
2018-12-12 09:56:34 +0100

    added 2018/12

---
+++ 2018/12/aoc2018_12_part1.py
@@ -0,0 +1,8 @@
+import re
+f=open("input.txt")
+def C(c):return int(c=='#')
+b=4*[0]
+s=b+map(C,f.readline()[15:].strip())+b
+m=dict((tuple(map(C,p)),C(r)) for p,r in re.findall('([.#]+) => ([.#])',f.read()))
+for g in xrange(20):s=b+[m[tuple(s[i:i+5])] for i in xrange(len(s)-5)]+b
+print sum(i-44 for i,x in enumerate(s) if x)
---
+++ 2018/12/aoc2018_12_part2.py
@@ -0,0 +1,11 @@
+import re
+f=open("input.txt")
+def C(c):return int(c=='#')
+b=500*[0]
+s=b+map(C,f.readline()[15:].strip())+b
+m=dict((tuple(map(C,p)),C(r)) for p,r in re.findall('([.#]+) => ([.#])',f.read()))
+r=0
+for g in xrange(200):
+ s=[0,0]+[m[tuple(s[i:i+5])] for i in xrange(len(s)-5)]+[0,0,0]
+ l,r=r,sum(i for i,x in enumerate(s) if x)
+print r+49999999800*(r-l)-500*sum(s)
