e8be7a27a4b9e3e8b427f8613d4f7c93736666d1
Martin Fiedler <keyj@emphy.de>
2022-12-18 21:04:57 +0100

    even more golf for 2022/18
    I oversaw a *very* obvious opportunity for saving another few bytes.

--- 2022/18/aoc2022_18_part2.py
+++ 2022/18/aoc2022_18_part2.py
@@ -1,5 +1,5 @@
 T,X=tuple,(0,1,2)
-G={tuple(map(int,l.split(',')))for l in open("input.txt")}
+G={T(map(int,l.split(',')))for l in open("input.txt")}
 A={0};q={T([-1]*3)}
 N=lambda s:(T(p[i]+d*(a==i)for i in X)for d in(-1,1)for a in X for p in s)
 while q:A|=q;q={z for z in N(q)if all(-2<c<22for c in z)}-A-G
