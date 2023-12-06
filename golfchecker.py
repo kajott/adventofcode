kajott/adventofcode

commit 6abe406d3571b8028308d118c06a5450e7791f4b

update golf rules
Turns out that recent Pythons don't like things like "1for" any longer :(

golfchecker.py
@@ -62,6 +62,13 @@ def trailwhite(data, lines):
 ###############################################################################
 
 @checker
+def extraspace(data, lines):
+    "extraneous whitespace"
+    return regex_checker(lines, r'(\s{2,})')
+
+###############################################################################
+
+@checker
 def indent(data, lines):
     "block more indented than necessary"
     stack = [0]
@@ -98,7 +105,7 @@ def mergelines(data, lines):
 ###############################################################################
 
 @checker
-def extraspace(data, lines):
+def tokenspace(data, lines):
     "extra space between tokens"
     def main(line):
         i, llen = 1, len(line) - 1
@@ -111,15 +118,12 @@ def extraspace(data, lines):
                 j += 1
             l = line[i-1]
             r = line[j]
-            if not((l in _es_left) and (r in _es_right)) \
-            and not((l+r) in ("0x", "0o", "0b")):
+            if not((l in _alnum) and (r in _alnum)):
                 yield (i, j)
             i = j
     return line_checker(lines, main)
-_alpha = "abcdefghijklmnopqrstuvwxyz"
-_alpha += _alpha.upper() + "_"
-_es_left = set(_alpha + " \t\r\n\f\v")
-_es_right = set(_alpha + "0123456789")
+_alnum = "abcdefghijklmnopqrstuvwxyz"
+_alnum = set(_alnum + _alnum.upper() + "_0123456789")
 
 ###############################################################################
 

commit e0ab50f10999b8c288a08ef8f5f38dbc4d1f4a08

fixed golf checker breakage introduced in 6abe406d

golfchecker.py
@@ -64,7 +64,7 @@ def trailwhite(data, lines):
 @checker
 def extraspace(data, lines):
     "extraneous whitespace"
-    return regex_checker(lines, r'(\s{2,})')
+    return regex_checker(lines, r'\S\s(\s+)\S')
 
 ###############################################################################
 
@@ -118,12 +118,13 @@ def tokenspace(data, lines):
                 j += 1
             l = line[i-1]
             r = line[j]
-            if not((l in _alnum) and (r in _alnum)):
+            if not((l in _ts_left) and (r in _ts_right)):
                 yield (i, j)
             i = j
     return line_checker(lines, main)
 _alnum = "abcdefghijklmnopqrstuvwxyz"
-_alnum = set(_alnum + _alnum.upper() + "_0123456789")
+_ts_right = set(_alnum + _alnum.upper() + "_0123456789")
+_ts_left = _ts_right | {" "}
 
 ###############################################################################
 